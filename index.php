26eb42547369289967f70de3241e07726223cea7
Samir Cibis <samir.cibis@tptlive.ee>
2026-09-04 09:43:05 +0300

    add math examples

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <?php
 $test = 10;
-$test = 3.1;
-$test = "Hello";
-$test = 'õpilane';
-$test = 'bruh';
-$test = true;
-$test = false;
-$test = null;
-$test = [1, true, 'blah'];
-$test = new stdClass();
-$test->name = 'John';
+$test = 10 + 10;
+$test = 10 - 10;
+$test = 10 * 10 * 10;
+$test = 10 / 10;
+$test = 5 % 2;
+$test = 2 ** 3;
+$test += 10;
+$test -= 10;
+$test *= 10;  
+$test /= 10;
+$test %= 10;
+$test **= 10;
+$test++;
+$test--;
+
+$a = 1;
+$b = ++$a;
+var_dump($a, $b);
+
+//$test = "Hello";
+//$test = 'õpilane';
+//$test = 'bruh';
+//$test = true;
+//$test = false;
+//$test = null;
+//$test = [1, true, 'blah'];
+//$test = new stdClass();
+//$test->name = 'John';
 var_dump($test);
