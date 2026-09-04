0527d4c0ae9de3136bb4b0fb04e21dfb41291e76
Samir Cibis <samir.cibis@tptlive.ee>
2026-09-04 09:29:21 +0300

    added test and explanations about int

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-echo "<h1>Hello, World!</h1>";
-?>
+$test = 10;
+$test = 3.1;
+$test = "Hello";
+$test = 'õpilane';
+$test = 'bruh';
+$test = true;
+$test = false;
+$test = null;
+$test = [1, true, 'blah'];
+$test = new stdClass();
+$test->name = 'John';
+var_dump($test);
