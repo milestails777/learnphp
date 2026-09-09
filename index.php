732b8347fdb725ba950915f3c20fb3cb9ff2afe6
Samir Cibis <samir.cibis@tptlive.ee>
2026-09-09 11:36:24 +0300

    9th september lesson

--- index.php
+++ index.php
@@ -1,16 +1,128 @@
 <?php
-$test = 'hello';
-$test = 'hello' . ' world' . '!';
-$test = $test . '!!!!!';
-$test .= '!!!!!';
-$age = 18;
-$name = 'Miles';
-$test = $name . ' is ' . $age . ' years old.';
-$test = "$name is $age years old.";
-$test = "hello\n\nworld";
-$test = <<<END
-Hello world
-END;
+$numbers = array(1, 2, 3);
+$numbers = [1, 2, 3];
+var_dump($numbers[1]);
+$numbers[2] = 5;
+var_dump($numbers);
+$test = [1, 'sasd', true, [1, 2, 3]];
+var_dump($test);
+var_dump($test[3][1]);
+$test = [
+    'name' => 'Miles Cibis',
+    'age' => 18,
+    1,
+    2,
+    3,
+    'color' => 'red',
+    'music' => 'jazz',
+    5,
+    100 => 6,
+    7,
+];
+var_dump($test);
+var_dump($test['name']);
+
+$combined = [1, 2, 3] + [4, 5, 6, 7];
+//[1, 2, 3] + 
+//[4, 5, 6, 7]; an example of array union operator in different method
+var_dump($combined);
+
+$text = implode('_', $test);
+var_dump($text);
+
+array_push($test, 77, 'asdasd', 243);
+var_dump($test);
+$test[] = 'value';
+unset($test[100]);
+var_dump($test);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--- Unused code below from the past classes ---
+//$test = 'hello';
+//$test = 'hello' . ' world' . '!';
+//$test = $test . '!!!!!';
+//$test .= '!!!!!';
+//$age = 18;
+//$name = 'Miles';
+//$test = $name . ' is ' . $age . ' years old.';
+//$test = "$name is $age years old.";
+//$test = "hello\n\nworld";
+//$test = <<<END
+//Hello world
+//END;
 
 //$test = 10;
 //$test = 10 + 10;
@@ -28,9 +140,9 @@ END;
 //$test++;
 //$test--;
 
-$a = 1;
-$b = ++$a;
-var_dump($a, $b);
+//$a = 1;
+//$b = ++$a;
+//var_dump($a, $b);
 
 //$test = "Hello";
 //$test = 'õpilane';
@@ -41,4 +153,4 @@ var_dump($a, $b);
 //$test = [1, true, 'blah'];
 //$test = new stdClass();
 //$test->name = 'John';
-var_dump($test);
+//var_dump($test);
