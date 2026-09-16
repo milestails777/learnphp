07e88ae3f255a1624dcdfb210657e861ea083c2e
Samir Cibis <samir.cibis@tptlive.ee>
2026-09-16 10:49:25 +0300

    add do while, foreach and recursive funcion example

--- index.php
+++ index.php
@@ -1,25 +1,55 @@
 <?php
  
- for($i = 0; $i < 10; $i++) {
-     var_dump($i);
- }
-
- for($i = 9; $i>=0; $i--) {
-     var_dump($i);
- }
-
- for($i = 1; $i < 1_000_000_000; $i*= 2) {
+for($i = 0; $i < 10; $i++) {
     var_dump($i);
- }
+}
 
- $time = time();
- $count = 0;
+for($i = 9; $i>=0; $i--) {
+    var_dump($i);
+}
+
+for($i = 1; $i < 1_000_000_000; $i*= 2) {
+    var_dump($i);
+}
+
+$time = time();
+$count = 0;
 while($time + 1 > time()) {
     $count++;
 }
 var_dump($count);
 
+while(false) {
+    var_dump('WHILE');
+}
 
+do {
+    var_dump('DO WHILE');
+} while(false);
+
+$fruits = ['apple', 'banana', 'cherry'];
+
+for($i = 0; $i < count($fruits); $i++) {
+    $fruit = $fruits[$i];
+    var_dump($fruit);
+}
+
+foreach($fruits as $fruit) {
+    var_dump($fruit);
+}
+
+foreach($fruits as $key => $fruit) {
+    var_dump($key, $fruit);
+}
+
+function recursive($i) {
+    if($i < 10) {
+        var_dump($i);
+        recursive($i + 1);
+    }
+}
+
+recursive(0);
 
 
 
