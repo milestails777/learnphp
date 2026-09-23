43784ca8448b546c49a2ca7ccbd636e8018e8179
Samir Cibis <samir.cibis@tptlive.ee>
2026-09-23 11:17:37 +0300

    add php templating example

--- index.php
+++ index.php
@@ -1,45 +1,27 @@
-<?php
-// library
+<?php 
+$name = 'Miles';
+?>
 
-class Job {
-    public function task(Logger $logger) {
-        for($i=0; $i<10; $i++) {
-            // we do some task
-            
-            $logger->log("Task $i completed!");
-        }
-    }
-}
-
-class ConsoleLogger {
-    public function log($message) {
-        echo "$message\n";
-    }
-}
-
-class NothingLogger implements Logger {
-    public function log($message) {
-        
-    }
-}
-
-interface Logger {
-    public function log($message);
-}
-
-//user code
-
-class FileLogger implements Logger {
-    public function log($message) {
-        $file = fopen('log.txt', 'a');
-        fwrite($file, "$message\n");
-        fclose($file);
-    }
-}
-
-$job = new job();
-$logger = new FileLogger();
-$job->task($logger);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello <?=$name?>!</h1>
+    <ul>
+        <?php for($i=0;$i<10;$i++): ?>
+            <?php if($i%2 === 0): ?>
+                <li style="color: green"><?=$i?></li>
+            <?php else: ?>
+                <li style="color: red"><?=$i?></li>
+            <?php endif ?>
+        <?php endfor ?>
+    </ul>
+</body>
+</html>
 
 
 
@@ -91,47 +73,5 @@ $job->task($logger);
 
 
 
-//--- Unused code below from the past classes ---
-//$test = 'hello';
-//$test = 'hello' . ' world' . '!';
-//$test = $test . '!!!!!';
-//$test .= '!!!!!';
-//$age = 18;
-//$name = 'Miles';
-//$test = $name . ' is ' . $age . ' years old.';
-//$test = "$name is $age years old.";
-//$test = "hello\n\nworld";
-//$test = <<<END
-//Hello world
-//END;
 
-//$test = 10;
-//$test = 10 + 10;
-//$test = 10 - 10;
-//$test = 10 * 10 * 10;
-//$test = 10 / 10;
-//$test = 5 % 2;
-//$test = 2 ** 3;
-//$test += 10;
-//$test -= 10;
-//$test *= 10;  
-//$test /= 10;
-//$test %= 10;
-//$test **= 10;
-//$test++;
-//$test--;
 
-//$a = 1;
-//$b = ++$a;
-//var_dump($a, $b);
-
-//$test = "Hello";
-//$test = 'õpilane';
-//$test = 'bruh';
-//$test = true;
-//$test = false;
-//$test = null;
-//$test = [1, true, 'blah'];
-//$test = new stdClass();
-//$test->name = 'John';
-//var_dump($test);
