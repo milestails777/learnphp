1b3afb4a7feaf9311d77e349293a278ffc9d2885
Samir Cibis <samir.cibis@tptlive.ee>
2026-09-25 10:01:42 +0300

    add router script

--- views/us.php
+++ views/us.php
@@ -1,35 +1,26 @@
 <?php
 
-//if(isset($title)) {
-//  echo $title;
-//} else {
-//  echo 'Default Title';
-//}
-
-//echo isset($title) ? $title : 'Default title';
-//echo $title ?? 'Default title';
-
 $posts = [
   [
-    'title' => 'Some World title 1',
+    'title' => 'Some US title 1',
     'date' => 'January 1, 2021',
     'author' => 'Pets',
     'body' => 'Some World body 1',
   ],
   [
-    'title' => 'Some World title 1',
+    'title' => 'Some US title 1',
     'date' => 'January 1, 2023',
     'author' => 'Sega',
     'body' => 'Some World body 2',
   ],
   [
-    'title' => 'Some World title 1',
+    'title' => 'Some US title 1',
     'date' => 'January 1, 2024',
     'author' => 'Mario',
     'body' => 'Some World body 3',
   ],
   [
-    'title' => 'Some World title 1',
+    'title' => 'Some US title 1',
     'date' => 'January 1, 2025',
     'author' => 'Verity',
     'body' => 'Some World body 4',
